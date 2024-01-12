9c1f8d78f1f3bd98f175f1d0da92b48ac1c3ac5d
syfxlin <i@ixk.me>
2024-01-12 21:29:01 +0800

    fix(config): Config not working

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
-import config from "@antfu/eslint-config";
+import antfu, { Awaitable, FlatConfigItem, OptionsConfig, UserConfigItem } from "@antfu/eslint-config";
 
-export default config({
-  formatters: true,
-  typescript: true,
-  stylistic: {
-    quotes: "double",
-    indent: 2,
-    semi: true,
-  },
-  rules: {
-    "curly": ["error", "multi-line", "consistent"],
-    "no-console": ["off"],
-    "style/brace-style": ["error", "1tbs"],
-    "style/member-delimiter-style": ["error", { multiline: { delimiter: "semi" }, singleline: { delimiter: "semi" } }],
-    "ts/ban-ts-comment": ["off"],
-    "ts/consistent-type-imports": ["off"],
-  },
-});
+export default function config(options?: OptionsConfig & FlatConfigItem, ...configs: Awaitable<UserConfigItem | UserConfigItem[]>[]): Promise<UserConfigItem[]> {
+  return antfu({
+    ...options,
+    formatters: true,
+    typescript: true,
+    stylistic: options?.stylistic === false
+      ? false
+      : {
+          quotes: "double",
+          indent: 2,
+          semi: true,
+          ...options?.stylistic === true ? {} : options?.stylistic,
+        },
+    rules: {
+      "curly": ["error", "multi-line", "consistent"],
+      "no-console": ["off"],
+      "style/brace-style": ["error", "1tbs"],
+      "style/member-delimiter-style": ["error", { multiline: { delimiter: "semi" }, singleline: { delimiter: "semi" } }],
+      "ts/ban-ts-comment": ["off"],
+      "ts/consistent-type-imports": ["off"],
+      ...options?.rules,
+    },
+  }, ...configs);
+}
